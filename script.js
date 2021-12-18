cebc8f8d04fb9fc40c0049f0e9431e21d76d309f
Oleksandr <90865871+Hidan25@users.noreply.github.com>
2021-12-18 00:12:15 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+//alert('Hello JS!');
+//const userName = prompt('Enter name');
+//console.log(userName);
+const userAge = Number(prompt('Enter your age'));
+console.log(userAge);
+//const nextAge = userAge + 1;
+//console.log(nextAge);
+if (userAge > 18){
+console.log('Good')
+}else{
+  console.log('not good')
+}
